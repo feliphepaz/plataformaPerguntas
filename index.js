4f1f4ee15a15272a0be46874b1f3905f446d7dab
Feliphe Paz <feliphepaz@MacBook-Air-de-Feliphe.local>
2022-06-21 07:52:18 -0300

    progresso no sistema

--- index.js
+++ index.js
@@ -1,18 +1,59 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+const connection = require("./database/database");
+const Pergunta = require("./database/Pergunta");
+
+connection
+  .authenticate()
+  .then(() => {
+    console.log("Conexão feita com sucesso");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-app.get("/:nome", (req, res) => {
-  const nome = req.params.nome;
-  const msgErro = false;
-  const frutas = ["Banana", "Maçã", "Pêra"];
-  res.render("index", {
-    nome,
-    idade: 22,
-    msgErro,
-    frutas,
+app.get("/", (req, res) => {
+  Pergunta.findAll({ raw: true, order: [["id", "DESC"]] }).then((perguntas) => {
+    res.render("index", {
+      perguntas,
+    });
+  });
+});
+
+app.get("/perguntar", (req, res) => {
+  res.render("perguntar");
+});
+
+app.get("/pergunta/:id", (req, res) => {
+  const id = req.params.id;
+  Pergunta.findOne({
+    where: { id: id },
+  }).then((pergunta) => {
+    console.log(pergunta);
+    if (pergunta !== null) {
+      res.render("pergunta", {
+        pergunta,
+      });
+    } else {
+      res.redirect("/");
+    }
+  });
+});
+
+app.post("/salvarpergunta", (req, res) => {
+  const titulo = req.body.titulo;
+  const descricao = req.body.descricao;
+  Pergunta.create({
+    titulo,
+    descricao,
+  }).then(() => {
+    res.redirect("/");
   });
 });
 
